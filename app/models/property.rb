9ef6c474cda47d4dcec04daee7bd9fa423543bb6
Rohan <rohan.farzana@mable.com.au>
2025-04-11 13:05:02 +1000

    add wishlists for user and save it

--- app/models/property.rb
+++ app/models/property.rb
@@ -6,7 +6,7 @@ class Property < ApplicationRecord
     
     has_many :wishlists, dependent: :destroy
 
-    has_many :wishlisted_users, through: :wishlist, source: :user, dependent: :destroy
+    has_many :wishlisted_users, through: :wishlists, source: :user, dependent: :destroy
     
     monetize :price_cents, allow_nil: true
     
@@ -22,5 +22,12 @@ class Property < ApplicationRecord
     def update_average_rating
         average_rating = reviews.average(:final_rating);
         update_column(:average_final_rating, average_rating)
+    end
+
+    def wishlisted_by?(user = nil)
+
+        return if user.nil?
+
+        wishlisted_users.include?(user)
     end
 end
