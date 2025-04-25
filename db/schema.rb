d025fa83946961049ede8c5ac07d60c8f16b1635
Rohan <rohan.farzana@mable.com.au>
2025-04-25 12:36:03 +1000

    add seeds to populate room guest part dynamically

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_23_053658) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_25_020710) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,10 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_23_053658) do
     t.string "price_currency"
     t.integer "reviews_count"
     t.decimal "average_final_rating"
+    t.integer "guest_count", default: 0
+    t.integer "bedroom_count", default: 0
+    t.integer "bed_count", default: 0
+    t.integer "bathroom_count", default: 0
   end
 
   create_table "reservations", force: :cascade do |t|
