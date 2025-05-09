e8536335c09df8d8c689596a70fe3ad62adc319a
Rohan <rohan.farzana@mable.com.au>
2025-05-09 15:33:18 +1000

    backend: overall_ratings

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,5 +1,6 @@
 class PropertiesController < ApplicationController
     def show
         @property = Property.find(params[:id])
+        @overall_review_count = @property.reviews.group("final_rating").count.transform_keys(&:to_i)
     end
 end
