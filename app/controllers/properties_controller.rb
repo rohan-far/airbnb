d113b050e7fa69da61571812676c26763d42d73a
Rohan <rohan.farzana@mable.com.au>
2025-05-27 14:27:04 +1000

    Optimisation

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,6 +1,11 @@
 class PropertiesController < ApplicationController
     def show
-        @property = Property.find(params[:id])
-        @overall_review_count = @property.reviews.group("final_rating").count.transform_keys(&:to_i)
+        @property = Property.includes(:reviews).find(params[:id])
+        @overall_review_count = @property
+                                .reviews
+                                .group("final_rating")
+                                .count
+                                .transform_keys(&:to_i)
+        @overall_review_count.default = 0
     end
 end
